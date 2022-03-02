47f409fe727d3634856c91c092d64c3357ec8c6f
Emincan Karasoy <emincannkarasoy@gmail.com>
2022-03-02 10:58:43 +0300

    Migrate activity splash screen to android splash screen

--- app/src/main/java/com/emincankarasoy/makeeasy/ui/MainActivity.kt
+++ app/src/main/java/com/emincankarasoy/makeeasy/ui/MainActivity.kt
@@ -2,7 +2,9 @@ package com.emincankarasoy.makeeasy.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.emincankarasoy.makeeasy.R
 import com.emincankarasoy.makeeasy.ui.view.ApplicationActivity
@@ -11,16 +13,12 @@ import kotlinx.coroutines.*
 class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        splashScreen.setKeepOnScreenCondition { true }
+        goToNextScreen()
+        finish()
 
-    override fun onResume() {
-        super.onResume()
-        lifecycleScope.launch(Dispatchers.Default) {
-            delay(4000L)
-            goToNextScreen()
-        }
     }
 
     private fun goToNextScreen(){
